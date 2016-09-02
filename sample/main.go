0266f55193b4402b3c5bcb0ffa71f96c1f0369cf
ieee0824 <ieee0824@ezweb.ne.jp>
2016-09-02 13:42:21 +0900

    add sample

--- sample/main.go
+++ sample/main.go
@@ -4,13 +4,42 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/goshinobi/client"
+	"github.com/goshinobi/client/ipchecker"
 )
+
+func init() {
+	ipchecker.RegistChecker(myIPChecker)
+}
+
+func myIPChecker(c *http.Client) string {
+	result := ""
+	url := "http://www.ugtop.com/spill.shtml"
+	resp, err := c.Get(url)
+	if err != nil {
+		return ""
+	}
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	doc.Find("table").Find("tbody").Each(func(i int, s *goquery.Selection) {
+		if i == 1 {
+			s.Find("tr").Each(func(i int, s *goquery.Selection) {
+				if i == 2 {
+					result = s.Find("font").Text()
+				}
+			})
+		}
+	})
+
+	return result
+}
 
 func main() {
 	c := client.New(true)
-	fmt.Println(c)
-	c.Add(&http.Client{}, nil)
 	fmt.Println(c)
 	c.Add(&http.Client{}, nil)
 	fmt.Println(c)

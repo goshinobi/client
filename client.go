452df7a275c6e83e96908880788070a898e843b7
shirase_aoi <ieee0824@ezweb.ne.jp>
2016-12-27 10:42:09 +0900

    http clientの生成

--- client.go
+++ client.go
@@ -47,4 +47,20 @@ func newClient(cfg ClientCfg) (*client, error) {
 }
 
 func NewClient(cfgs ...ClientCfg) *Client {
+	ret := Client{
+		0,
+		make([]*client, 0, len(cfgs)),
+	}
+	for _, cfg := range cfgs {
+		c, err := newClient(cfg)
+		if err != nil {
+			continue
+		}
+		ret.list = append(ret.list, c)
+	}
+	return &ret
+}
+
+func (c *Client) Len() int {
+	return len(c.list)
 }
